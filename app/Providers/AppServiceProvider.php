064ad67aa4fdba901d7a8f420cddd3564899f0f0
necro <necroalone@gmail.com>
2019-11-23 10:05:03 +0700

    7. Опишите “проброс” класса TestOne  в класс TestBasicService  в контейнер-провайдере AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\TestBasicService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(TestBasicService::class, function ()   {
+            return new TestBasicService(new \App\Services\TestOne('none', false, 0));
+        });
+        $this->app->alias(TestBasicService::class, 'service.testbasic');
+
     }
 
     /**
